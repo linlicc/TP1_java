d3c5fdafee3690fd742fd32eedd96a604daa29f9
aline <alineferj@outlook.com.br>
2020-10-28 00:42:22 -0300

    teste classe

--- src/br/edu/infnet/Application.java
+++ src/br/edu/infnet/Application.java
@@ -3,6 +3,10 @@ package br.edu.infnet;
 public class Application {
 
 	public static void main(String[] args) {
+		// pegar os dados do construtor na classe Aluno
+		  Aluno aluno = new Aluno("ALine", 1234);
+		  
+	
 		// TODO Auto-generated method stub
 
 	}
