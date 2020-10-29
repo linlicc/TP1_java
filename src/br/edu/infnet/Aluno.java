ee259949d1e0142d556ef7ef473e8d3692a45c37
aline <alineferj@outlook.com.br>
2020-10-29 00:57:11 -0300

    cadastro

--- src/br/edu/infnet/Aluno.java
+++ src/br/edu/infnet/Aluno.java
@@ -1,49 +1,57 @@
 package br.edu.infnet;
 
-    // definição de aluno (escopo)
+// definição de aluno (escopo)
 
 public class Aluno {
 
 	// SEG quando n quiser que os dados sejam acessados = private
-	
+
 	private int matricula;
-    private String nome;
-    private float[] notas = new float [3];
-    
-    // criando o construtor da classe (definindo parâmetros)
-    
-    public Aluno(String nome, int matricula) {
-    	this.nome = nome; 
-        this.matricula = matricula;
-        
-    	
-    }
-   
-    
+	private String nome;
+	private Float[] notas = new Float[3];
+
+	// criando o construtor da classe (definindo parâmetros)
+
+	public Aluno(String nome, int matricula) {
+		this.nome = nome;
+		this.matricula = matricula;
+
+	}
+
+	public void cadastrarNota(Float nota) {
+		
+// valor inicial, final e o incrementador 
+        for (int i = 0; i < 2; i++) {
+			if (notas[i] == null) {
+                 this.notas[i] = nota; 
+			}
+
+		}
+
+	}
+
 	public int getMatricula() {
 		return matricula;
 	}
+
 	public void setMatricula(int matricula) {
 		this.matricula = matricula;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public float[] getNotas() {
+
+	public Float[] getNotas() {
 		return notas;
 	}
-	public void setNotas(float[] notas) {
+
+	public void setNotas(Float[] notas) {
 		this.notas = notas;
 	}
-    
-    
-    
-    
-    
-    
-    
 
 }
--- src/br/edu/infnet/Application.java
+++ src/br/edu/infnet/Application.java
@@ -1,14 +1,75 @@
 package br.edu.infnet;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// pegar os dados do construtor na classe Aluno
-		  Aluno aluno = new Aluno("ALine", 1234);
-		  
-	
-		// TODO Auto-generated method stub
+		List<Aluno> turma = new ArrayList<Aluno>();
+
+		boolean executando = true;
+		while (executando) {
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("1 - Registrar as notas de um novo aluno:");
+			System.out.println("2 - Consultar Boletins de um aluno:");
+			System.out.println("3 - Consultar notas da turma:");
+			System.out.println("4 - Sair:");
+
+			int opcaoSelecionada = sc.nextInt();
+
+			switch (opcaoSelecionada) {
+
+			case (1):
+
+				// cadastrar aluno
+
+				System.out.println("Informe o nome do aluno");
+				String nome = sc.next();
+
+				System.out.println("Informe a matrícula do aluno");
+				int matricula = sc.nextInt();
+				Aluno novoAluno = new Aluno(nome, matricula);
+
+				System.out.println("Informe a nota 1");
+				Float nota1 = sc.nextFloat();
+				novoAluno.cadastrarNota(nota1);
+
+				System.out.println("Informe a nota 2");
+				Float nota2 = sc.nextFloat();
+				novoAluno.cadastrarNota(nota2);
+
+				System.out.println("Informe a nota 3");
+				Float nota3 = sc.nextFloat();
+				novoAluno.cadastrarNota(nota3);
+
+				turma.add(novoAluno);
+
+				break;
+
+			case (2):
+				break;
+
+			case (3):
+				break;
+
+			case (4):
+				executando = false;
+				break;
+
+			}
+		}
+		for (Aluno aluno : turma) {
+			System.out.println(aluno.getNome());
+
+			Float notas[] = aluno.getNotas();
+			for (int i = 0; i < 2; i++) {
+				System.out.println(notas[i].toString());
+			}
+
+		}
 
 	}
-
 }
